dda932764ffe152d701bf863d93b03259c20762c
unknown <aslan.thr@gmail.com>
2023-08-29 11:18:48 -0400

    config for deploy

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-import { useState } from "react";
 import Header from "../components/Header";
 import AddRestaurant from "../components/AddRestaurant";
 import RestaurantList from "../components/RestaurantList";
